ac93454253ec6489b65ae70e5ad39142c4bae966
Miel Vander Sande <miel.vandersande@meemoo.be>
2023-08-25 11:31:33 +0200

    Fix Docker setup

--- Dockerfile
+++ Dockerfile
@@ -16,10 +16,10 @@ COPY ./requirements.txt /code/requirements.txt
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 
 # 
-COPY ./fasterid /app
+COPY ./ /app
 RUN chown -R appuser:appgroup /app
 
 USER appuser
 
 # 
-CMD ["uvicorn", "fasterid:app", "--host", "0.0.0.0", "--port", "8080"]
+CMD ["uvicorn", "fasterid.fasterid:app", "--host", "0.0.0.0", "--port", "8080"]
--- fasterid/database.py
+++ fasterid/database.py
@@ -8,7 +8,12 @@ if settings.sqlalchemy_database_url is None:
     database_url = f"sqlite:///./{settings.fasterid_filename}"
 else:
     database_url = settings.sqlalchemy_database_url
-engine = create_engine(database_url, connect_args={"check_same_thread": False})
+
+connect_args = {}
+if database_url.startswith("sqlite://"):
+    connect_args = {"check_same_thread": False}
+
+engine = create_engine(database_url, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
