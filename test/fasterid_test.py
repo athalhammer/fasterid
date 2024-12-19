d0547c0c0eb145317968aebf72d47e11542859f3
Andreas Thalhammer <andreas@thalhammer.bayern>
2024-12-19 22:26:11 +0100

    black for tests

--- test/fasterid_test.py
+++ test/fasterid_test.py
@@ -10,11 +10,13 @@ from rdflib import Graph
 
 client = TestClient(app)
 
+
 def test_basic():
     response = client.post("/")
     assert response.status_code == 201
     assert isinstance(response.json(), dict)
     assert "@id" in response.json().keys()
+
 
 def test_multiple():
     response = client.post("/", json={"number": 5})
@@ -22,13 +24,17 @@ def test_multiple():
     assert isinstance(response.json(), list)
     assert len(response.json()) == 5
 
+
 def test_content_negotiation():
-    response = client.post("/", headers={'accept': 'application/ld+json'})
+    response = client.post("/", headers={"accept": "application/ld+json"})
     assert response.status_code == 201
     assert response.headers["content-type"] == "application/ld+json"
 
+
 def test_json_ld():
-    response = client.post("/", json={"number": 5}, headers={'accept': 'application/ld+json'})
+    response = client.post(
+        "/", json={"number": 5}, headers={"accept": "application/ld+json"}
+    )
     assert response.status_code == 201
     g = Graph().parse(data=response.content, format="json-ld")
     assert len(g) == 5
